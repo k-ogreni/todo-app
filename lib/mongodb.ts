ec32398f2980bfee00b9d26d4f56e524227b80f4
Kristina <k.ogreni15@gmail.com>
2025-06-08 22:55:26 +0200

    fix for build errors

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,15 +4,26 @@ const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) throw new Error('MONGODB_URI not defined');
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongooseCache: MongooseCache | undefined;
+}
+
+const cached = globalThis.mongooseCache ?? (globalThis.mongooseCache = { conn: null, promise: null });
 
 async function connectToDatabase() {
   if (cached.conn) return cached.conn;
+
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
       bufferCommands: false,
     });
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
